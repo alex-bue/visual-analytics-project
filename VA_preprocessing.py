ea4bc96e1117d5afbe790d97d089df9b6a976c9f
julianthr <julian.thier@gmx.de>
2022-12-14 15:17:32 +0100

    added data export, date column format

--- VA_preprocessing.py
+++ VA_preprocessing.py
@@ -2,42 +2,44 @@ import pandas as pd
 import numpy as np
 from geopy.geocoders import Nominatim
 from geopy.point import Point
+import datetime as dt
 
 # read in all single files and concatenate them
-path = '../../Documents/Master/Semester1/Visual_Analytics/Dashboard_Files/'
-df = pd.DataFrame()
-for i in [["type=fixed/", "fixed"], ["type=mobile/", "mobile"]]:
-    path_i = path + i[0]
-    category = i[1]
-    for j in [['year=2019/', 2019], ['year=2020/', 2020], ['year=2021/', 2021], ['year=2022/', 2022]]:
-        path_j = path_i + j[0]
-        year = j[1]
-        if year != 2022:
-        # ToDO: Date format
-            for k in [['quarter=1/', '01'], ['quarter=2/', '04'], ['quarter=3/', '07'], ['quarter=4/', '10']]:
-                path_k = path_j + k[0]
-                month = k[1]
-                path_k = path_k + str(year)+"-"+month+"-01_performance_"+str(category)+"_tiles.parquet"
-                test_raw = pd.read_parquet(path_k, engine='pyarrow')
-                test_short = test_raw.head().copy()
-                # add year, month and category information to the dataframe - those are
-                test_short['year'] = year
-                test_short['quarter_month'] = month
-                test_short['category'] = category
-                df = pd.concat([df, test_short])
-        elif year == 2022:
-            for k in [['quarter=1/', '01'], ['quarter=2/', '04'], ['quarter=3/', '07']]:
-                path_k = path_j + k[0]
-                month = k[1]
-                path_k = path_k + str(year) + "-" + month + "-01_performance_" + str(category) + "_tiles.parquet"
-                test_raw = pd.read_parquet(path_k, engine='pyarrow')
-                test_short = test_raw.head().copy()
-                # add year, month and category information to the dataframe - those are
-                test_short['year'] = year
-                test_short['quarter_month'] = month
-                test_short['category'] = category
-                df = pd.concat([df, test_short])
+# path = '../../Documents/Master/Semester1/Visual_Analytics/Dashboard_Files/'
+# df = pd.DataFrame()
+# for i in [["type=fixed/", "fixed"], ["type=mobile/", "mobile"]]:
+#     path_i = path + i[0]
+#     category = i[1]
+#     for j in [['year=2019/', 2019], ['year=2020/', 2020], ['year=2021/', 2021], ['year=2022/', 2022]]:
+#         path_j = path_i + j[0]
+#         year = j[1]
+#         if year != 2022:
+#         # ToDO: Date format
+#             for k in [['quarter=1/', '01'], ['quarter=2/', '04'], ['quarter=3/', '07'], ['quarter=4/', '10']]:
+#                 path_k = path_j + k[0]
+#                 month = k[1]
+#                 path_k = path_k + str(year)+"-"+month+"-01_performance_"+str(category)+"_tiles.parquet"
+#                 test_raw = pd.read_parquet(path_k, engine='pyarrow')
+#                 test_short = test_raw.head().copy()
+#                 # add year, month and category information to the dataframe - those are
+#                 test_short['quarter'] = dt.date(year, int(month), 1)
+#                 test_short['category'] = category
+#                 df = pd.concat([df, test_short])
+#         elif year == 2022:
+#             for k in [['quarter=1/', '01'], ['quarter=2/', '04'], ['quarter=3/', '07']]:
+#                 path_k = path_j + k[0]
+#                 month = k[1]
+#                 path_k = path_k + str(year) + "-" + month + "-01_performance_" + str(category) + "_tiles.parquet"
+#                 test_raw = pd.read_parquet(path_k, engine='pyarrow')
+#                 test_short = test_raw.head().copy()
+#                 # add year, month and category information to the dataframe - those are
+#                 test_short['quarter'] = dt.date(year, int(month), 1)
+#                 test_short['category'] = category
+#                 df = pd.concat([df, test_short])
 
+df_raw = pd.read_parquet('../../Documents/Master/Semester1/Visual_Analytics/Dashboard_Files/type=fixed/'
+                         'year=2019/quarter=1/2019-01-01_performance_fixed_tiles.parquet', engine = 'pyarrow')
+df = df_raw.head()
 # retrieve lat-long data from tile column
 df['tile'] = df['tile'].apply(lambda x: x.replace("POLYGON", ""))
 df['tile'] = df['tile'].apply(lambda x: x.replace("(", ""))
@@ -50,8 +52,29 @@ df = df.drop(columns=['rest', 'tile', 'quadkey'])
 geolocator = Nominatim(user_agent="geoapiExercises")
 # ToDo: use np.vectorize
 df['location'] = df.apply(lambda x: geolocator.reverse(Point(x['lat'], x['long'])), axis=1)
-# df['location'] = df.apply(lambda x: str(x['lat'])+','+str(x['long']), axis=1)
 df['country'] = df['location'].apply(lambda x: x.raw['address'])
 df['country'] = df['country'].apply(lambda x: x.get('country', np.nan))
-# ToDo: add country filtering
+
+
+# save the whole dataset as csv:
+df.to_csv('../../Documents/Master/Semester1/Visual_Analytics/df_whole_world.csv', sep=",")
+
+# get a list of all countries in geopy:
+unique_countries = df['country'].unique()
+unique_countries = np.sort(unique_countries)
+# np.savetxt('../../Documents/Master/Semester1/Visual_Analytics/country_list.csv', unique_countries, delimiter=",")
+
+# filter for Europe:
+europe = pd.read_csv('../../Documents/Master/Semester1/Visual_Analytics/Europe_2.csv')
+europe = europe['Name'][:50].tolist()
+
+df_europe = df[df['country'].isin(europe)]
+df_europe.to_csv('../../Documents/Master/Semester1/Visual_Analytics/df_europe.csv', sep=",")
+
+# filter for Germany
+df_germany = df_europe[df_europe['country'] == 'Germany']
+df_germany.to_csv('../../Documents/Master/Semester1/Visual_Analytics/df_germany.csv', sep=",")
+
 print()
+
+
