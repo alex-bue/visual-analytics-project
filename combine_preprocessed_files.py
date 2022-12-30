4e2bac693ee9fec358569121664c830584c4493f
Alexander Büscher <alibue@hotmail.de>
2022-12-30 18:10:13 +0100

    Add sep to read_csv and commented out dropna's

--- combine_preprocessed_files.py
+++ combine_preprocessed_files.py
@@ -3,19 +3,19 @@ import datetime as dt
 
 start = dt.datetime.now()
 # choose whole_world, europe or germany
-level = 'germany'
+level = 'whole_world'
 
-path = '../../Documents/Master/Semester1/Visual_Analytics/'
-# path = './data/performance/'
+# path = '../../Documents/Master/Semester1/Visual_Analytics/'
+path = './data/performance/'
 
 combined_df = pd.DataFrame()
 for i in range(0, 30):
-    df = pd.read_csv(path + f'preprocessed_files/{level}/{level}_{i}.csv')
+    df = pd.read_csv(path + f'preprocessed_files/{level}/{level}_{i}.csv', sep=';')
 
     # remove all NA's
-    df = df.dropna(subset={'name'})
+    # df = df.dropna(subset='name'})
 
-    combined_df = pd.concat(combined_df, df)
+    combined_df = pd.concat([combined_df, df])
 
 combined_df.to_csv(path + f'{level}_final.csv')
 
