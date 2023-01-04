97c68915c6d8c65f263506a56ae39fb9a2e7f56f
julianthr <julian.thier@gmx.de>
2023-01-04 20:47:43 +0100

    added functionality that keeps columns in wide format in germany_wide_to_long.py

--- germany_wide_to_long.py
+++ germany_wide_to_long.py
@@ -7,13 +7,20 @@ df_germany = pd.read_csv("./data/final_data/germany_final.csv", sep=";")
 # drop na (maybe add do at the end of germamy_reverse_geocoding script before export)
 df_germany = df_germany.dropna()
 
+# create duplicates of columns - these will be used by the visuals that need wide format (and no long format)
+df_germany['avg_d_mbps_copy'] = df_germany['avg_d_mbps']
+df_germany['avg_u_mbps_copy'] = df_germany['avg_u_mbps']
+df_germany['avg_lat_ms_copy'] = df_germany['avg_lat_ms']
+
 # Rename value vars to look better for dashboard
-df_germany.rename(columns={'avg_d_mbps':'Download Speed', 'avg_u_mbps':'Upload Speed', 'avg_lat_ms':'Latency'}, inplace=True)
+df_germany.rename(columns={'avg_d_mbps': 'Download Speed', 'avg_u_mbps': 'Upload Speed', 'avg_lat_ms': 'Latency'},
+                  inplace=True)
 
 # Convert DataFrame from wide to long format
-# id vars: federal_state, county, municipality, category, tests, devices, quarter
-# value vars: avg_d_mbps, avg_u_mbps, avg_lat_ms
-df_germany_long = pd.melt(df_germany, id_vars=['federal_state', 'county', 'municipality', 'category', 'quarter'], value_vars=['Download Speed', 'Upload Speed', 'Latency'], var_name='measure', value_name='value')
-
+id_vars_list = ['federal_state', 'county', 'municipality', 'category', 'quarter',
+                'avg_d_mbps_copy', 'avg_u_mbps_copy', 'avg_lat_ms_copy']
+value_vars_list = ['Download Speed', 'Upload Speed', 'Latency']
+df_germany_long = pd.melt(df_germany, id_vars=id_vars_list, value_vars=value_vars_list,
+                          var_name='measure', value_name='value')
 # Write to csv
 df_germany_long.to_csv("./data/final_data/germany_final_long.csv", sep=";", index=False)
